3ac22c159947bd20130a8251f65217051c42f583
Peng <1183868638@qq.com>
2018-12-03 11:21:41 +0800

    New File

---
+++ when.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+browser=webdriver.Chrome()
+browser.get('https://desktop.mt365.alpha.tmogroup.asia')
+browser.maximize_window()
+logoname=browser.find_element_by_class_name("site-logo").text
+print (logoname)
+if logoname=='Glyco365':
+ print ('登陆成功')
+else :
+ raise NameError ('登录失败!')
