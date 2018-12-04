4d9292dcd954a5a0f6e3810d8048dc921e828508
Peng <1183868638@qq.com>
2018-12-04 15:52:53 +0800

    Re-start MT365 ON Jenkins!

--- mt365.py
+++ mt365.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 browser=webdriver.Chrome()
-browser.get('https://desktop.mt365.alpha.tmogroup.asia')
+browserA.ge('https://desktop.mt365.alpha.tmogroup.asia')
 browser.maximize_window()
 logoname=browser.find_element_by_class_name("site-logo").text
 print (logoname)
 if logoname=='Glyco366':
- print ('登陆成功')
+ print ('登陆成功.GET是错误的！')
 else :
  raise NameError ('登录失败啦啦啦!')
