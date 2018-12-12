8a51a301df4955667950f50f9cc9a0b6ce569598
Peng <1183868638@qq.com>
2018-12-12 15:56:56 +0800

    Continue Jenkins Again!

--- mt365.py
+++ mt365.py
@@ -4,7 +4,7 @@ browser.get('https://desktop.mt365.alpha.tmogroup.asia')
 browser.maximize_window()
 logoname=browser.find_element_by_class_name("site-logo").text
 print (logoname)
-if logoname=='Glyco366':
- print ('登陆成功.GET是错误的！')
+if logoname=='Glyco365':
+ print ('登陆成功！')
 else :
  raise NameError ('登录失败啦啦啦!')
